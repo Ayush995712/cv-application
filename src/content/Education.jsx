d07310f11907cca046e4cf32e807ecaf1a140c29
Ayush995712 <ayush181217@gmail.com>
2025-09-05 18:43:59 +0530

    Added save, cancel, delete buttons

--- src/content/Education.jsx
+++ src/content/Education.jsx
@@ -1,8 +1,22 @@
 import { useState } from "react";
 
-export function Education({setCollege, setDegree, setStartDate, setEndDate, setLocation}) {
-
+export function Education({setCollege, setDegree, setStartDate, setEndDate, setLocation, college, degree, startDate, endDate, location, setEducationDetails}) {
     const [open, setOpen] = useState(false);
+    const [addEducation, setAddEducation] = useState(false);
+
+    function saveEducation() {
+        setEducationDetails(prev => [...prev, 
+            {
+                college: college,
+                degree: degree,
+                startDate: startDate,
+                endDate: endDate,
+                location: location
+            }
+        ])
+        setOpen(true);
+        setAddEducation(false);
+    }
 
     return (
         <div className="w-[400px] h-auto bg-blue-400 rounded-lg p-[12px] shadow-xl shadow-blue-250">
@@ -15,44 +29,63 @@ export function Education({setCollege, setDegree, setStartDate, setEndDate, setL
             open ? "rotate-180" : "rotate-0"}`} />
                 </div>
             </div>
-            {open && (
             <div className="flex flex-col gap-[4px]">
-                <div>
-                    <div className="text-white">College</div>
-                    <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
-                        setCollege(e.target.value);
-                    }} />
-                </div>
-                <div>
-                    <div className="text-white">Degree</div>
-                    <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
-                        setDegree(e.target.value);
-                    }} />
-                </div>
-                <div className="flex gap-[6px]">
-                    <div className="flex flex-col">
-                        <div className="text-white">Start Date</div>
+            {addEducation && (
+                (
+                    <div>
+                    <div>
+                        <div className="text-white">College</div>
                         <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
-                            setStartDate(e.target.value);
+                            setCollege(e.target.value);
+                            college = e.target.value;
                         }} />
                     </div>
-                    <div className="flex flex-col">
-                        <div className="text-white">End Date</div>
+                    <div>
+                        <div className="text-white">Degree</div>
                         <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
-                            setEndDate(e.target.value);
+                            setDegree(e.target.value);
+                            degree = e.target.value;
                         }} />
                     </div>
-                </div>
-                <div>
-                    <div className="text-white">Location</div>
-                    <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
-                        setLocation(e.target.value);
-                    }} />
-                </div>
-                <div className="flex justify-center items-center">
-                    <button className=" border border-gray-300 text-cyan-100 rounded-3xl px-3 py-2 focus:outline-none cursor-pointer mt-[8px]">Add Education &#43;</button>
-                </div>
-            </div>)}
+                    <div className="flex gap-[6px]">
+                        <div className="flex flex-col">
+                            <div className="text-white">Start Date</div>
+                            <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                                setStartDate(e.target.value);
+                                startDate = e.target.value;
+                            }} />
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="text-white">End Date</div>
+                            <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                                setEndDate(e.target.value);
+                                endDate = e.target.value;
+                            }} />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="text-white">Location</div>
+                        <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                            setLocation(e.target.value);
+                            location = e.target.value;
+                        }} />
+                    </div>
+                    <div className="flex justify-between">
+                        <button className="border border-gray-300 text-cyan-100 rounded-3xl px-3 py-2 focus:outline-none cursor-pointer mt-[4px]" onClick={saveEducation} >Save</button>
+                        <div>
+                            <button className="border border-gray-300 text-cyan-100 rounded-3xl px-3 py-2 focus:outline-none cursor-pointer mt-[4px]">Cancel</button>
+                            <button className="border border-gray-300 text-cyan-100 rounded-3xl px-3 py-2 focus:outline-none cursor-pointer mt-[4px]">Delete</button>
+                        </div>
+                    </div>
+                </div>)
+                )}
+                {open && <div className="flex justify-center items-center">
+                    <button className="border border-gray-300 text-cyan-100 rounded-3xl px-3 py-2 focus:outline-none cursor-pointer mt-[8px]" onClick={() => {
+                        setAddEducation(true);
+                        setOpen(false)
+                    }} >Add Education &#43;</button>
+                </div>}
+            </div>
         </div>
     )
 }

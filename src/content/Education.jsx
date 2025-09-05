77f4b8a804cc97f7f73f550434be7aed1b4aed9f
Ayush995712 <ayush181217@gmail.com>
2025-09-05 20:26:53 +0530

    Updated save education logic

--- src/content/Education.jsx
+++ src/content/Education.jsx
@@ -1,32 +1,52 @@
 import { useState } from "react";
 
-export function Education({setCollege, setDegree, setStartDate, setEndDate, setLocation, college, degree, startDate, endDate, location, setEducationDetails}) {
+export function Education({setCollege, setDegree, setStartDate, setEndDate, setLocation, college, degree, startDate, endDate, location, educationDetails,  setEducationDetails}) {
     const [open, setOpen] = useState(false);
     const [addEducation, setAddEducation] = useState(false);
+    const [selectedEducation, setSelectedEducation] = useState(null);
 
     function saveEducation() {
-        setEducationDetails(prev => [...prev, 
-            {
-                college: college,
-                degree: degree,
-                startDate: startDate,
-                endDate: endDate,
-                location: location
-            }
-        ])
+        if(selectedEducation) {
+            setEducationDetails(prev => 
+                prev.map(el => 
+                    el.college === selectedEducation.college ? {college, degree, startDate, endDate, location} : el
+                )
+            );
+            setSelectedEducation(null);
+        } else {
+            setEducationDetails(prev => [...prev, 
+                {college, degree, startDate, endDate, location}
+            ])
+        }
         setOpen(true);
         setAddEducation(false);
     }
 
     return (
         <div className="w-[400px] h-auto bg-blue-400 rounded-lg p-[12px] shadow-xl shadow-blue-250">
-            <div className="text-white font-bold text-lg/7 cursor-pointer flex justify-between" onClick={() => {
-                setOpen(!open)
-            }} >
-                <div>Education</div>
-                <div>
-                    <img src="https://img.icons8.com/?size=100&id=85327&format=png&color=000000" alt="icon" className={`w-5 h-5 transform transition-transform duration-100 ${
-            open ? "rotate-180" : "rotate-0"}`} />
+            <div className="flex flex-col">
+                <div className="text-white font-bold text-lg/7 cursor-pointer flex justify-between" onClick={() => {
+                    setOpen(!open)
+                }} >
+                    <div>Education</div>
+                    <div>
+                        <img src="https://img.icons8.com/?size=100&id=85327&format=png&color=000000" alt="icon" className={`w-5 h-5 transform transition-transform duration-100 ${
+                open ? "rotate-180" : "rotate-0"}`} />
+                    </div>
+                </div>
+                <div className="flex flex-col rounded-lg gap-[4px]">
+                    {open && educationDetails.map((element, index) => (
+                        <div key={index} className="w-full border border-cyan-200 rounded-lg text-xl font-semibold bg-[beige] cursor-pointer" onClick={() => {
+                            setSelectedEducation(element);
+                            setCollege(element.college);
+                            setDegree(element.degree);
+                            setStartDate(element.startDate);
+                            setEndDate(element.endDate);
+                            setLocation(element.location);
+                            setAddEducation(true)
+                            setOpen(false);
+                        }} >{element.college}</div>
+                    ))}
                 </div>
             </div>
             <div className="flex flex-col gap-[4px]">
@@ -35,14 +55,14 @@ export function Education({setCollege, setDegree, setStartDate, setEndDate, setL
                     <div>
                     <div>
                         <div className="text-white">College</div>
-                        <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                        <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" value={college} onChange={(e) => {
                             setCollege(e.target.value);
                             college = e.target.value;
                         }} />
                     </div>
                     <div>
                         <div className="text-white">Degree</div>
-                        <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                        <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" value={degree} onChange={(e) => {
                             setDegree(e.target.value);
                             degree = e.target.value;
                         }} />
@@ -50,14 +70,14 @@ export function Education({setCollege, setDegree, setStartDate, setEndDate, setL
                     <div className="flex gap-[6px]">
                         <div className="flex flex-col">
                             <div className="text-white">Start Date</div>
-                            <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                            <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" value={startDate} onChange={(e) => {
                                 setStartDate(e.target.value);
                                 startDate = e.target.value;
                             }} />
                         </div>
                         <div className="flex flex-col">
                             <div className="text-white">End Date</div>
-                            <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                            <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" value={endDate} onChange={(e) => {
                                 setEndDate(e.target.value);
                                 endDate = e.target.value;
                             }} />
@@ -65,7 +85,7 @@ export function Education({setCollege, setDegree, setStartDate, setEndDate, setL
                     </div>
                     <div>
                         <div className="text-white">Location</div>
-                        <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => {
+                        <input type="text" className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" value={location} onChange={(e) => {
                             setLocation(e.target.value);
                             location = e.target.value;
                         }} />

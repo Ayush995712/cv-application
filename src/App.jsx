04c50b9e89975d92d96990d74f94e3884b97b55b
Ayush995712 <ayush181217@gmail.com>
2025-09-08 10:06:15 +0530

    Added experience array and functionality to save button

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
   const [description, setDescription] = useState('');
   const [loadExample, setLoadExample] = useState(true);
   const [educationDetails, setEducationDetails] = useState([]);
+  const [experience, setExperience] = useState([]);
 
   return (
     <>
@@ -53,7 +54,7 @@ function App() {
         </div> : <div>
           {showContent && <Education college={college} setCollege={setCollege} degree={degree} setDegree={setDegree} startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} location={location} setLocation={setLocation} educationDetails={educationDetails} setEducationDetails={setEducationDetails} />}
         </div>}
-        {showContent && <Experience company={company} setCompany={setCompany} positionTitle={positionTitle} setPositionTitle={setPositionTitle} startDateInCompany={startDateInCompany} endDateInCompany={endDateInCompany} locationOfWork={locationOfWork} setLocationOfWork={setLocationOfWork} description={description} setDescription={setDescription} />}
+        {showContent && <Experience company={company} setCompany={setCompany} positionTitle={positionTitle} setPositionTitle={setPositionTitle} startDateInCompany={startDateInCompany} setStartDateInCompany={setStartDateInCompany} endDateInCompany={endDateInCompany} setEndDateInCompany={setEndDateInCompany} locationOfWork={locationOfWork} setLocationOfWork={setLocationOfWork} description={description} setDescription={setDescription} experience={experience} setExperience={setExperience} />}
       </div>
       <div className='main-cv'>
         {loadExample ? <CV name={"Jon Doe"} email={"jondoe12@gmail.com"} phoneNumber={"+91 7898234782"} address={"Durgapur, West Bengal"} college={college} degree={degree} startDate={startDate} endDate={endDate} location={location} educationDetails={educationDetails} setEducationDetails={setEducationDetails} /> : <CV name={name} email={email} phoneNumber={phoneNumber} address={address} college={college} degree={degree} startDate={startDate} endDate={endDate} location={location} educationDetails={educationDetails} setEducationDetails={setEducationDetails} />}
--- src/content/Experience.jsx
+++ src/content/Experience.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react"
 
-export function Experience({company, setCompany, positionTitle, setPositionTitle, startDateInCompany, setStartDateInCompany, endDateInCompany, setEndDateInCompany, locationOfWork, setLocationOfWork, description, setDescription}) {
+export function Experience({company, setCompany, positionTitle, setPositionTitle, startDateInCompany, setStartDateInCompany, endDateInCompany, setEndDateInCompany, locationOfWork, setLocationOfWork, description, setDescription, experience, setExperience}) {
     const [open, setOpen] = useState(false);
-    const [addExperience, setAddExperience] = useState(false);
+    const [addExperienceBtn, setAddExperienceBtn] = useState(false);
+
+    function saveExperience() {
+        setExperience(prev => [...prev, {
+            company, positionTitle, startDateInCompany, endDateInCompany, locationOfWork, description
+        }]);
+        console.log(experience);
+        setAddExperienceBtn(false);
+        setOpen(true);
+    }
 
     return (
         <div className="w-[400px] h-auto bg-blue-400 rounded-lg p-[12px] shadow-xl shadow-blue-250">
-            <div className="text-white font-bold text-lg/7 cursor-pointer flex justify-between" onClick={() => {
-                setOpen(!open);
-                {addExperience ? setAddExperience(false) : setAddExperience(addExperience)};
-                }} >
-                <div>Experience</div>
-                <div>
-                    <img src="https://img.icons8.com/?size=100&id=85327&format=png&color=000000" alt="icon" className={`w-5 h-5 transform transition-transform duration-100 ${open ? "rotate-180" : "rotate-0"}`} />
+            <div>
+                <div className="text-white font-bold text-lg/7 cursor-pointer flex justify-between" onClick={() => {
+                    setOpen(!open);
+                    {addExperienceBtn ? setAddExperienceBtn(false) : setAddExperienceBtn(addExperienceBtn)};
+                    }} >
+                    <div>Experience</div>
+                    <div>
+                        <img src="https://img.icons8.com/?size=100&id=85327&format=png&color=000000" alt="icon" className={`w-5 h-5 transform transition-transform duration-100 ${open ? "rotate-180" : "rotate-0"}`} />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-[6px]">
+                    {open && experience.map((e, index) => (
+                        <div key={index}>{e.company}</div>
+                        ))}
                 </div>
             </div>
-            {addExperience && <div className="flex flex-col gap-[4px]">
+            {addExperienceBtn && <div className="flex flex-col gap-[4px]">
                 <div>
                     <div>Company Name</div>
                     <div>
@@ -54,10 +70,19 @@ export function Experience({company, setCompany, positionTitle, setPositionTitle
                         <textarea className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Write the description here ..."></textarea>
                     </div>
                 </div>
+                <div className="flex justify-between">
+                    <div>
+                        <button className="border border-gray-300 px-[8px] py-[6px] rounded-2xl cursor-pointer" onClick={saveExperience}>Save</button>
+                    </div>
+                    <div>
+                        <div>Cancel</div>
+                        <div>Delete</div>
+                    </div>
+                </div>
             </div>}
             {open && <div className="flex justify-center items-center">
                 <button className="border border-gray-300 rounded-3xl px-[12px] py-[8px] cursor-pointer text-cyan-100" onClick={() => {
-                    setAddExperience(true);
+                    setAddExperienceBtn(true);
                     setOpen(false);
                     }}>Add Experience &#43;</button>
             </div>}
